463081e58558c07a8dbcd1775855bfda48e2d8e5
nishitha09 <97939491+nishitha09@users.noreply.github.com>
2022-02-18 12:05:12 +0530

    Add files via upload

---
+++ SumofDiagonals/file comparison/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+
+namespace file_comparison
+{
+    class fileRead
+    {
+        public static void Main()
+        {
+            string file1;
+            string file2;
+            Console.Write("Enter the first file path");
+            file1 = Console.ReadLine();
+            Console.Write("Enter the second file path:");
+            file2 = Console.ReadLine();
+            if (!File.Exists(file1))
+            {
+                Console.WriteLine("First file doesnot exit!");
+            }
+            else if (!File.Exists(file2))
+                {
+                Console.WriteLine("Second fi;e doesnt exist!");
+            }
+            else if (File.ReadAllText(file1) == File.ReadAllText(file2))
+            {
+                Console.WriteLine("Both files contain the same content");
+            }
+            else
+            {
+                Console.WriteLine("Contents of files are not same");
+            }
+        }
+    }
+}
+

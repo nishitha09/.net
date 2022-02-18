97fa6b52d50ebdf1236e9766bf7e1b9ce0a1abc3
nishitha09 <97939491+nishitha09@users.noreply.github.com>
2022-02-18 12:03:15 +0530

    Add files via upload

---
+++ FrequencyIS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FrequencyIS
+{
+    class FrequencyIS
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string inputString;
+            Console.WriteLine("\n____Frequency of word 'is'____");
+            Console.Write("\n Enter the input string:");
+            inputString = Console.ReadLine();
+            char[] separator = { ',',' ','.', '!', '\n' };
+            string testString = inputString.ToLower();
+            string[] outcomes = testString.Split(separator);
+            foreach(String s in outcomes)
+                {
+                Console.WriteLine(s);
+                if (s == "is")
+                    count++;
+                    }
+            Console.WriteLine("\n Number of 'is' in '"+inputString+"' is: "+count);
+           
+        }
+    }
+}

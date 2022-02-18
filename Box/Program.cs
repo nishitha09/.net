4654634d05a4fc6f41091a202e0b016c621a3e87
nishitha09 <97939491+nishitha09@users.noreply.github.com>
2022-02-18 12:00:07 +0530

    Add files via upload

---
+++ Box/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Box
+{
+    class Box
+    {
+        float width;
+        float height;
+        float length;
+        public float volume
+        {
+            get { return width * height * length; }
+        }
+        public Box(float width, float height, float length)
+        { 
+            this.width = width;
+            this.height = height;
+            this.length = height;
+            }
+        public static float operator +(Box box1, Box box2)
+        {
+            return box1.volume + box2.volume;
+        }
+        public override string ToString()
+        {
+        return "box with width"+ width+", height" +height+"and length"+length;
+}
+}
+class operatoroverloading
+    {
+    public static void Main()
+{
+    Box box1 = new Box(10, 20, 30);
+    Box box2 = new Box(25, 32, 15);
+    Console.WriteLine("Volume of {0} is:{1}", box1, box1.volume);
+            Console.WriteLine("Volume of {0} is:{1}", box2, box2.volume);
+            Console.WriteLine("Volume after adding boxes: {0}", box1+box2);  
+      }
+    }
+}

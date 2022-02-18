cc59d51abfb165962defaa4430299ff80ac78bb9
nishitha09 <97939491+nishitha09@users.noreply.github.com>
2022-02-18 11:50:39 +0530

    Add files via upload

---
+++ BINARY TRIANGLE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BINARY_TRIANGLE
+{
+    class binarytriangle
+    {
+        static void Main(string[] args)
+        {
+            int number, digit = 1;
+            Console.Write("/n Enter the number of lines:");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i=1;i<=number;i++)
+            {
+              for (int space = number-i; space>0; space--)
+            {
+                Console.Write("");
+            }
+            for (int j = 0; j<i; j++)
+            {
+                Console.Write(digit + "");
+                digit=(digit==1) ? 0 : 1;
+            }
+            Console.Write("\n");
+        }
+           
+    }
+    }
+}

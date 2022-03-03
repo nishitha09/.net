fd96a136a3226559cb4cc89f3d4dcb5874e9778c
nishitha09 <97939491+nishitha09@users.noreply.github.com>
2022-03-03 11:53:03 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,21 @@
 ﻿using System;
-public class PrimeNumberExample
+public class PalindromeExample
 {
     public static void Main(string[] args)
     {
-        int n, i, m = 0, flag = 0;
-        Console.Write("Enter the Number to check Prime: ");
+        int n, r, sum = 0, temp;
+        Console.Write("Enter the Number: ");
         n = int.Parse(Console.ReadLine());
-        m = n / 2;
-        for (i = 2; i <= m; i++)
+        temp = n;
+        while (n > 0)
         {
-            if (n % i == 0)
-            {
-                Console.Write("Number is not Prime.");
-                flag = 1;
-                break;
-            }
+            r = n % 10;
+            sum = (sum * 10) + r;
+            n = n / 10;
         }
-        if (flag == 0)
-            Console.Write("Number is Prime.");
+        if (temp == sum)
+            Console.Write("Number is Palindrome.");
+        else
+            Console.Write("Number is not Palindrome");
     }
 }
